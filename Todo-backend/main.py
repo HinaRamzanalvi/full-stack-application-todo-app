06b0b7e4198de742afa981e1431c3a2180d68e6b
HinaRamzanalvi <princess4177812@gmail.com>
2026-01-15 20:35:25 +0500

    addinG

--- Todo-backend/main.py
+++ Todo-backend/main.py
@@ -11,10 +11,11 @@ app = FastAPI(title="Hackathon II Todo API", version="1.0.0")
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # In production, replace with specific origins
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=[
+        "https://hackathon-todo-frontend-hs084165q-hina-alvi.vercel.app/"
+    ],  # In production, replace with specific origins
+   
+ 
 )
 
 # Include API routes
